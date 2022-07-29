7ef36b4c4e7231f911f47635d00e01f05bdb4312
grj <3303272844@qq.com>
2022-07-29 17:23:33 +0800

    button组件的封装

--- button/src/App.vue
+++ button/src/App.vue
@@ -1,17 +1,25 @@
 <template>
-  <div></div>
+  <div>
+    <Buttton title="主要按钮" type="primary" @myclick="Fn"></Buttton><br />
+    <Buttton title="成功按钮" type="success" @myclick="Fn"></Buttton><br />
+    <Buttton title="警告按钮" type="warning" @myclick="Fn"></Buttton><br />
+    <Buttton title="危险按钮" type="danger" @myclick="Fn"></Buttton><br />
+    <Buttton title="信息按钮" type="message" @myclick="Fn"></Buttton><br />
+  </div>
 </template>
 
 <script>
+import Buttton from '@/components/Button.vue'
 export default {
-  data() {
-    return {}
+  components: {
+    Buttton
   },
-
-  created() {},
-
-  methods: {}
+  methods: {
+    Fn() {
+      alert(111)
+    }
+  }
 }
 </script>
 
-<style scoped lang="less"></style>
+<style></style>
---
+++ button/src/components/Button.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <button :class="`one-button-${type}`" @click="clickFn">
+      {{ title }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '主要按钮'
+    },
+    type: {
+      type: String,
+      default: 'primary',
+      // 自动校验
+      validator(val) {
+        // console.log(val)
+        return ['primary', 'success', 'warning', 'danger', 'message'].includes(
+          val
+        )
+      }
+    }
+    // size: {
+    //   type: String,
+    //   default: 'normal',
+    //   validator(val) {
+    //     console.log(val)
+    //     return ['normal', 'small', 'mini'].includes(val)
+    //   }
+    // }
+  },
+  methods: {
+    clickFn() {
+      this.$emit('myclick')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+button {
+  color: #fff;
+  outline: none;
+  border: none;
+  padding: 8px 24px;
+  border-radius: 4px;
+  font-size: 18px;
+}
+.one-button-primary {
+  background-color: rgb(31, 165, 31);
+}
+.one-button-success {
+  background-color: hotpink;
+}
+.one-button-warning {
+  background-color: rgb(207, 79, 50);
+}
+.one-button-danger {
+  background-color: rgba(255, 0, 0, 0.954);
+}
+.one-button-message {
+  background-color: rgb(72, 131, 190);
+}
+// .normal {
+//   size: 32px;
+// }
+// .small {
+//   size: 24px;
+// }
+// .mini {
+//   size: 14px;
+// }
+</style>
